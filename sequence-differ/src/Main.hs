d55287bd1ad9c079dca9f9c1ed4fb1d129b5ecd2
Ryan James Spencer <spencer.ryanjames@gmail.com>
2015-11-25 15:19:16 +1100

    Remove comments and make  a helper function.

--- sequence-differ/src/Main.hs
+++ sequence-differ/src/Main.hs
@@ -2,7 +2,7 @@
 
 sequence-differ
 
-A very simple tool to compare and suggest changes between two lists
+A diff utility written in Haskell
 
 Authors:
 Ryan James Spencer
@@ -16,11 +16,6 @@ main = undefined
 data ChangeType = Replace | Insert | Delete
 type Change = (ChangeType, Int)
 
--- Checks to see if two sequences have the same contents
--- If they do, we add that to our list
--- If not, we recursively perform our lcs on all of seq a and the rest of b
--- and all of seq b and the rest of a
--- we choose the longest of the two to return
 lcs :: Eq a => [a] -> [a] -> [a]
 lcs [] _ = []
 lcs _ [] = []
