0381973439bc4cc68a02107796b1257cc9813c60
Ryan James Spencer <spencer.ryanjames@gmail.com>
2015-11-25 14:37:31 +1100

    Added initial code slightly modified from Wikipedia / Rosetta Code entries.

--- sequence-differ/src/Main.hs
+++ sequence-differ/src/Main.hs
@@ -10,30 +10,22 @@ Sean Donald Spencer
 
 -}
 
--- Note: `undefined` has a polymorphic type and can be put anywhere
-
--- This just says main is of type Input/Output 'action'. ()
--- stands for 'unit' and usually is akin to 'void'
--- it is where we'll put all our potentially 'impure' code
 main :: IO ()
 main = undefined
 
--- Haskell is strongly typed, therefore, unlike dynamically typed
--- languages, we use types to express a large part of our program's
--- functionality. Since types can be statically checked, this means
--- we really want to create types as we can guarantee runtime behavior
--- by simply compiling our program. Type theorists like to call this
--- property as being 'an expressive type system', which means our types
--- can express a lot of properties, both to us as humans and also
--- to the program as guarantees.
---
--- We use the 'data' keyword to define a type.
--- The pipe symbol denotes 'or', e.g. data Color = Red | Green | Blue
 data ChangeType = Replace | Insert | Delete
--- A change is a tuple with a type of change and an index location
 type Change = (ChangeType, Int)
 
--- This says 'matcher takes a list of as and a list of bs and
--- gives us back a list of changes
-matcher :: [a] -> [b] -> [Change]
-matcher (x:xs) (y:ys) = undefined
+-- Checks to see if two sequences have the same contents
+-- If they do, we add that to our list
+-- If not, we recursively perform our lcs on all of seq a and the rest of b
+-- and all of seq b and the rest of a
+-- we choose the longest of the two to return
+lcs :: Eq a => [a] -> [a] -> [a]
+lcs [] _ = []
+lcs _ [] = []
+lcs a@(ah:arst) b@(bh:brst)
+    | ah == bh = ah : lcs arst brst
+    | otherwise = longest (lcs a brst) (lcs arst b)
+    where longest :: [a] -> [a] -> [a]
+          longest xs ys = if length xs > length ys then xs else ys
