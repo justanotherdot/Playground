4f4b8756054b8ac9f8943db19206841654d4f4a0
Ryan James Spencer <spencer.ryanjames@gmail.com>
2015-12-08 08:47:28 +1100

    Translation of orc-battle into haskell.
    Original orc-battle written by Conrad Barski, can be found at...
    http://landoflisp.com/orc-battle.lisp

--- watch.sh
+++ watch.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# !!!Requires inotify-tools!!!
+# Checks a directory for changes and runs actions based on, e.g. saves.
+#
 
 while inotifywait -qq -e modify `pwd`; do
     ./main.ml
