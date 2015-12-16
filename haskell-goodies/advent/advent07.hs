cb8916b8a38e5d792638402d52a9b467ff9e0ace
Ryan James Spencer <spencer.ryanjames@gmail.com>
2015-12-17 07:48:29 +1100

    A lot closer to using a better internally memoized variant.

--- haskell-goodies/advent/advent07.hs
+++ haskell-goodies/advent/advent07.hs
@@ -19,46 +19,55 @@ import Debug.Trace
 
 type Op = String
 type Ident = String
-type RuleMap = Map.Map Ident Rule
-data Rule = Rule Ident Op [Rule] | Value CUShort | Var String deriving (Show, Eq)
+type Circuit = Map.Map String CUShort
+data Arg = Value CUShort | Var Ident deriving (Show, Eq)
+data Rule = Assign Ident Arg
+          | And Ident Arg Arg
+          | Or Ident Arg Arg
+          | Lshift Ident Arg Arg
+          | Rshift Ident Arg Arg
+          | Not Ident Arg deriving (Show, Eq)
 
 
 main = do (fn:x:_) <- getArgs
           contents <- readFile fn
           let rs = rules contents
-          print $ evaluate x rs
+              c  = makeCircuit rs
+          print (c Map.! x)
+
+t = "123 -> x\n456 -> y\nx AND y -> d\nx OR y -> e\nx LSHIFT 2 -> f\ny RSHIFT 2 -> g\nNOT x -> h\nNOT y -> i"
+
+arg :: String -> Arg
+arg s = if isDigit (head s) then Value (read s :: CUShort)
+                            else Var s
 
 
 rule :: String -> Rule
-rule s = let elems   = words s
-             ident   = last elems
-             nthel i = elems !! i
-             in case length elems of
-                    5 -> Rule ident (nthel 1) (args [nthel 0, nthel 2]) -- Binary ops: AND, etc.
-                    4 -> Rule ident (nthel 0) (args [nthel 1])          -- "NOT"
-                    3 -> Rule ident "ASSIGN" (args [nthel 0])
-                    _ -> error "Unrecognized rule"
-                    where args = map arg
-                                 where arg :: String -> Rule
-                                       arg a = case dropWhile isDigit a of
-                                                   "" -> Value (read a :: CUShort)
-                                                   _  -> Var a
+rule s = let elems = words s
+             name  = last elems
+             in case length elems of 5 -> case elems !! 1 of "AND"    -> And name (arg $ head elems) (arg $ elems !! 2)
+                                                             "OR"     -> Or name (arg $ head elems) (arg $ elems !! 2)
+                                                             "LSHIFT" -> Lshift name (arg $ head elems) (arg $ elems !! 2)
+                                                             "RSHIFT" -> Lshift name (arg $ head elems) (arg $ elems !! 2)
+                                     4 -> Not name (arg $ elems !! 1)
+                                     3 -> Assign name (arg $ head elems)
 
-rules :: String -> RuleMap
-rules s = let ls = lines s
-              ruleList = map rule ls
-              ruleMap  = foldl (\x r@(Rule n _ _) -> Map.insert n r x) Map.empty
-              in ruleMap ruleList
+rules :: String -> [Rule]
+rules s = map rule $ lines s
 
 
-evaluate :: Ident -> RuleMap -> CUShort
-evaluate r m = case Map.lookup r m of
-                   Just (Rule _ "ASSIGN" (a:_))   -> parse a
-                   Just (Rule _ "AND" (a:b:_))    -> parse a .&. parse b
-                   Just (Rule _ "OR" (a:b:_))     -> parse a .|. parse b
-                   Just (Rule _ "LSHIFT" (a:b:_)) -> shift (parse a) (fromIntegral $ parse b)
-                   Just (Rule _ "RSHIFT" (a:b:_)) -> shift (parse a) (fromIntegral $ parse b)
-                   Just (Rule _ "NOT" (a:_))      -> complement $ parse a
-                   Just r                         -> error $ "Unrecognized rule under evaluation: " ++ show r
-                   where parse r = case r of Value n -> n
-                                             Var x   -> evaluate x m
+makeCircuit :: [Rule] -> Circuit
+makeCircuit rls = circuit
+          where
+                circuit :: Map.Map Ident CUShort
+                circuit = Map.fromList (map connect rls)
+                connect :: Rule -> (Ident, CUShort)
+                connect (Assign id val) = (id, get val)
+                connect (And id a1 a2) = (id, get a1 .&. get a2)
+                connect (Or id a1 a2) = (id, get a1 .|. get a2)
+                connect (Lshift id a val) = (id, shift (get a) (fromIntegral (get val)))
+                connect (Rshift id a val) = (id, shift (get a) (fromIntegral (get val)))
+                connect (Not id a) = (id, complement (get a))
+                get :: Arg -> CUShort
+                get (Var x) = circuit Map.! x
+                get (Value n) = n
