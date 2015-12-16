fd3cbbe7e8c02c79eb0a6171bb29cd8eb50683e7
Ryan James Spencer <spencer.ryanjames@gmail.com>
2015-12-17 08:55:07 +1100

    Tidy up and extra commentary to advent07.hs

--- haskell-goodies/advent/advent07.hs
+++ haskell-goodies/advent/advent07.hs
@@ -2,6 +2,12 @@
 
 My stab at the adventofcode.com's day 7 puzzle 'Some Assembly Required'
 
+This solution takes advantage of Haskell's built-in memoization (this is what the white paper
+"Implementation of the Spineless G-Tag Machine" is talking about.)
+
+Other Haskell solutions to this puzzle involve imperative-styled solutions (say with Data.HashMap.Strict)
+Typically this involves some hashmap (in particular, Data.HashMap.Strict)
+
 Ryan James Spencer
 16 Dec 2015
 
@@ -9,18 +15,14 @@ Ryan James Spencer
 
 import Data.Bits
 import Data.Char
-import Data.List
+import Data.Map.Lazy ((!))
 import Foreign.C.Types
 import System.Environment
-import qualified Data.Map.Lazy as Map
-import Data.Map.Lazy ((!))
-
--- for debugging only, will remove later XXX
-import Debug.Trace
+import qualified Data.Map.Lazy as M
 
 type Op = String
 type Ident = String
-type Circuit = Map.Map String CUShort
+type Circuit = M.Map String CUShort
 data Arg = Value CUShort | Var Ident deriving (Show, Eq)
 data Rule = Assign Ident Arg
           | And Ident Arg Arg
@@ -36,33 +38,29 @@ main = do (fn:x:_) <- getArgs
               c  = makeCircuit rs
           print (c ! x)
 
-t = "123 -> x\n456 -> y\nx AND y -> d\nx OR y -> e\nx LSHIFT 2 -> f\ny RSHIFT 2 -> g\nNOT x -> h\nNOT y -> i"
-rs = rules t
-
 arg :: String -> Arg
 arg s = if isDigit (head s) then Value (read s :: CUShort)
                             else Var s
 
-
 rule :: String -> Rule
 rule s = let elems = words s
              name  = last elems
-             in case length elems of 5 -> case elems !! 1 of "AND"    -> And name (arg $ head elems) (arg $ elems !! 2)
-                                                             "OR"     -> Or name (arg $ head elems) (arg $ elems !! 2)
-                                                             "LSHIFT" -> Lshift name (arg $ head elems) (arg $ elems !! 2)
-                                                             "RSHIFT" -> Rshift name (arg $ head elems) (arg $ elems !! 2)
-                                     4 -> Not name (arg $ elems !! 1)
-                                     3 -> Assign name (arg $ head elems)
+             in case length elems of
+                    5 -> case elems !! 1 of
+                              "AND"    -> And name (arg $ head elems) (arg $ elems !! 2)
+                              "OR"     -> Or name (arg $ head elems) (arg $ elems !! 2)
+                              "LSHIFT" -> Lshift name (arg $ head elems) (arg $ elems !! 2)
+                              "RSHIFT" -> Rshift name (arg $ head elems) (arg $ elems !! 2)
+                    4 -> Not name (arg $ elems !! 1)
+                    3 -> Assign name (arg $ head elems)
 
 rules :: String -> [Rule]
 rules s = map rule $ lines s
 
-
 makeCircuit :: [Rule] -> Circuit
 makeCircuit rls = circuit
-          where
-                circuit :: Map.Map Ident CUShort
-                circuit = Map.fromList (map connect rls)
+          where circuit :: M.Map Ident CUShort
+                circuit = M.fromList (map connect rls)
                 connect :: Rule -> (Ident, CUShort)
                 connect (Assign id val) = (id, get val)
                 connect (And id a1 a2) = (id, get a1 .&. get a2)
